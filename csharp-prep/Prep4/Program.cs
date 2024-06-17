cca67e360c6793d9873d3c88219ed0998a3351e2
Konstantin Bolotov <bkn@cloit.net>
2024-06-17 12:58:01 +0300

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    { 
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        float avg = 0;
+        int largest = 0;
+            
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        while (number != 0)
+        { 
+            numbers.Add(number); 
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > largest)
+            {
+                largest = num;
+            }
+            avg = (float)sum / numbers.Count;
+        }
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Average: " + avg);
+        Console.WriteLine("Largest number: " + largest);
     }
 }
