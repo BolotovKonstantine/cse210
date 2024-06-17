f154c1c6c75a834a04becad9d2854ce85f5faa20
Konstantin Bolotov <bkn@cloit.net>
2024-06-17 12:08:39 +0300

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        Boolean guess = false;
+        while (guess == false)
+        {
+            Console.WriteLine("Guess the number:");
+            int guessNumber = Convert.ToInt32(Console.ReadLine());
+            if (guessNumber == number)
+            {
+                guess = true;
+                Console.WriteLine("You guessed it");
+            }
+            else if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
