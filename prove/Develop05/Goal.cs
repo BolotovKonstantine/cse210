82b1e31498a97e1333dd97930c7d96ac6bf40c4f
Konstantin Bolotov <bkn@cloit.net>
2024-07-11 16:57:03 +0300

    Add Goal and Square classes
    Introduced a new abstract 'Goal' class, with its constructor and methods. Also, added the 'Square' subclass inheriting the 'Shape' class with 'GetArea' override method. These are primordial base classes for further development.

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,22 @@
+﻿public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string shortName, string description, string points)
+    {
+        _shortName = shortName;
+        _points = points;
+        _description = description;
+    }
+
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public abstract string GetStringRepresentation();
+
+    public string GetDetailsString()
+    {
+        return $"Goal: {_shortName}\nDescription: {_description}\nPoints: {_points}";
+    }
+}
